d5c3c1c0ea602cdb4f36b27ad995e618a0defa09
Milo <posadamolina@gmail.com>
2024-05-30 18:22:47 -0400

    Basics Set Up

--- main.py
+++ main.py
@@ -1,90 +1,4 @@
-# import pygame
-# import sys
-# from pygame.locals import*
-# from OpenGL.GLU import*
-# from OpenGL.GL import*
-#
-# vertex_path = "/Users/cposada/Desktop/Terminal/Rubiks_Cube/Cube_Model/vertices.txt"
-# face_path = "/Users/cposada/Desktop/Terminal/Rubiks_Cube/Cube_Model/faces.txt"
-#
-# paints = [
-# (253,254,255), #white
-# (206,221,48), #yellow
-# (195,2,3), #red
-# (255,97,14), #orange
-# (43,198,0), #green
-# (0,80,173), #blue
-# ]
-#
-# def get_list(txtname):
-#     listname = []
-#     with open(txtname) as f:
-#         for line in f:
-#             line = line.rstrip(",\r\n").replace("(",'').replace(")","").replace(" ",'')
-#             row = list(line.split(","))
-#             listname.append(row)
-#         listname = [[float(j) for j in i] for i in listname]
-#         return listname
-#
-# modelVerts = get_list(vertex_path)
-# modelFaces = get_list(face_path)
-#
-# def drawfaces():
-#     glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT) #clears each frame
-#     glClear(GL_TRIANGLES) #drawing method
-#     for eachface in (modelFaces):
-#         for eachvert in eachface:
-#             glColor3fv(paints[0]) #?
-#             glColor3fv(modelVerts[int(eachvert)])
-#         glEnd()
-#
-# def main():
-#     pygame.init()
-#     display = (1000, 1000) #window
-#     pygame.display.set_caption("Rubik's CFOP")
-#     FPS = pygame.time.Clock() #fps sync
-#     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-#     gluPerspective(45,1,.1,50)
-#     glTranslate(0,-1,-5) #xyz
-#     glRotate(0,1,0,0)
-#
-#     Left = False
-#     Right = False
-#
-#     def moveOBJ():
-#         if Left:
-#             glRotate(-1,0,1,0)
-#         if Right:
-#             glRotate(1,0,1,0)
-#
-#     while True:
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 pygame.quit()
-#                 sys.exit()
-#             if event.type == KEYDOWN:
-#                 if event.key == K_ESCAPE:
-#                     pygame.quit()
-#                     sys.exit()
-#                 if event.key == K_a:
-#                     Left = True
-#                 if event.key == K_d:
-#                     Right = True
-#             if event.type == KEYUP:
-#                 if event.key == K_a:
-#                     Left = False
-#                 if event.key == K_d:
-#                     Right = False
-#         pygame.display.flip()
-#         drawfaces()
-#         moveOBJ()
-#         FPS.tick(60)
-# main()
-
-
-
-
-### GPT CODE
+import os
 import pygame
 from pygame.locals import *
 from OpenGL.GL import *
@@ -94,25 +8,101 @@ from objloader import OBJ
 # Initialize Pygame and create a window
 pygame.init()
 display = (800, 600)
+pygame.display.set_caption("Rubik's CFOP")
 pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
-glTranslatef(0.0, 0.0, -5)
+gluPerspective(45, 1, .1, 50)
+glTranslatef(0,0,-5) #Not sure why this needs to be here
 
 # Load the model
-cube = OBJ("Cube_Model\Rubiks_Model.obj")
+current_dir = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(current_dir, "Cube_Model", "Rubiks_Model.obj")
+cube = OBJ(model_path)
 
-# Main loop
+def init_opengl():
+    glEnable(GL_DEPTH_TEST)  # Enable depth testing for correct Z-ordering
+    glEnable(GL_CULL_FACE)   # Enable face culling for performance
+    glCullFace(GL_BACK)      # Cull back faces
+init_opengl()
+
+
+# Control variables
+camera_rot = [0, 0]
+dragging = False
+last_mouse_pos = [0, 0]
+
+def handle_camera_dragging(): #ugly motion
+    global camera_rot, dragging, last_mouse_pos
+    if dragging:
+        mouse_pos = pygame.mouse.get_pos()
+        dx = mouse_pos[0] - last_mouse_pos[0]
+        dy = mouse_pos[1] - last_mouse_pos[1]
+        camera_rot[0] += dy * .08
+        camera_rot[1] += dx * .08
+        last_mouse_pos = mouse_pos
+
+
+Left = False
+Right = False
+Up = False
+Down = False
+
+def moveOBJ():
+    if Left:
+        glRotate(2,0,2,0)
+    if Right:
+        glRotate(-2,0,2,0)
+    if Up:
+        glRotate(2,2,0,0)
+    if Down:
+        glRotate(-2,2,0,0)
+
+
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
 
-    glRotatef(1, 3, 1, 1)  # Rotate the model for demonstration purposes
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-    # Draw the model
-    cube.draw()
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+            if event.key == K_a:
+                Left = True
+            if event.key == K_d:
+                Right = True
+            if event.key == K_w:
+                Up = True
+            if event.key == K_s:
+                Down = True
+        if event.type == KEYUP:
+            if event.key == K_a:
+                Left = False
+            if event.key == K_d:
+                Right = False
+            if event.key == K_w:
+                Up = False
+            if event.key == K_s:
+                Down = False
+        elif event.type == MOUSEBUTTONDOWN:
+            if event.button == 1:  # Left mouse button
+                dragging = True
+                last_mouse_pos = pygame.mouse.get_pos()
+        elif event.type == MOUSEBUTTONUP:
+            if event.button == 1:  # Left mouse button
+                dragging = False
+                camera_rot = [0, 0]
+        elif event.type == MOUSEMOTION:
+            if dragging:
+                handle_camera_dragging()
 
+#dont know what these are doing here
+    # glLoadIdentity()
+    # glTranslatef(0.0, 0.0, -5)
+
+    glRotatef(camera_rot[0], 1, 0, 0)
+    glRotatef(camera_rot[1], 0, 1, 0)
+
+    moveOBJ()
+    cube.draw()
     pygame.display.flip()
     pygame.time.wait(10)
--- objloader.py
+++ objloader.py
@@ -1,9 +1,19 @@
+from OpenGL.GL import *
+import os
+
 class OBJ:
     def __init__(self, filename):
         self.vertices = []
         self.faces = []
+        self.colors = []
+        self.materials = {}
 
+        self.load(filename)
+
+    def load(self, filename):
+        path = os.path.dirname(filename)
         with open(filename, 'r') as file:
+            material = None
             for line in file:
                 if line.startswith('v '):
                     parts = line.strip().split()
@@ -12,11 +22,30 @@ class OBJ:
                 elif line.startswith('f '):
                     parts = line.strip().split()
                     face = [int(parts[1].split('/')[0]), int(parts[2].split('/')[0]), int(parts[3].split('/')[0])]
-                    self.faces.append(face)
+                    self.faces.append((face, material))
+                elif line.startswith('usemtl '):
+                    material = line.split()[1]
+                elif line.startswith('mtllib '):
+                    self.load_mtl(os.path.join(path, line.split()[1]))
+
+    def load_mtl(self, filename):
+        with open(filename, 'r') as file:
+            material = None
+            for line in file:
+                if line.startswith('newmtl '):
+                    material = line.split()[1]
+                    self.materials[material] = {'Kd': [1, 1, 1]}  # Default to white
+                elif line.startswith('Kd ') and material:
+                    parts = line.strip().split()
+                    self.materials[material]['Kd'] = [float(parts[1]), float(parts[2]), float(parts[3])]
 
     def draw(self):
-        glBegin(GL_TRIANGLES)
-        for face in self.faces:
+        for face, material in self.faces:
+            if material and material in self.materials:
+                glColor3fv(self.materials[material]['Kd'])
+            else:
+                glColor3f(0, 0, 0)  # Default to white if no material
+            glBegin(GL_TRIANGLES)
             for vertex in face:
                 glVertex3fv(self.vertices[vertex - 1])
-        glEnd()
+            glEnd()
