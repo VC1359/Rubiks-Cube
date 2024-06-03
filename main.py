a8fc7c9bf9ed61477d3f7ed29300ef62e3397a3b
Milo <posadamolina@gmail.com>
2024-06-03 17:22:11 -0400

    .

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import bpy
 import math
-import reset
 
 # Creates a collection (for faces)
 def create_collection(name):
@@ -14,7 +13,7 @@ def add_to_collection(obj, collection_name):
     if collection:
         collection.objects.link(obj)
 #        bpy.context.scene.collection.objects.unlink(obj)
-        
+
 # Groups faces and create collections
 def define_collections():
     create_collection('Right')
@@ -23,15 +22,14 @@ def define_collections():
     create_collection('Back')
     create_collection('Up')
     create_collection('Down')
-#    create_collection('Middle')
-#    create_collection('Side')
-#    create_collection('Equator')
+    create_collection('Middle')
+    create_collection('Side')
+    create_collection('Equator')
 
 
 # Manual Init
 def start():
     define_collections()
-    
     # Group the cubes into collections
     for obj in bpy.data.objects:
         if obj.type == 'MESH':
@@ -47,7 +45,22 @@ def start():
                 add_to_collection(obj, 'Up')
             elif obj.location.z <= -.5:
                 add_to_collection(obj, 'Down')
-#start()
+
+# Function needs to be finished           
+def scramble(scramble): # Input should look like "D2 R' F" 
+    pass
+#    for move in range(0,len(scramble)-1):
+#        if scramble[move+1] == ' ':
+#            rotate_face(scramble[move])
+#        else if scramble[move+1] == '2' or scramble[move+1] == "'":
+#            rotate_face(scramble[move])
+#            
+#        else if s
+
+def reset_collections():
+    for collection in bpy.context.scene.collection.children:
+        if collection != bpy.data.collections.get('Solved_State'):
+            bpy.data.collections.remove(collection)
 
 angle = math.pi / 2
 def get_rotation_params(face):
@@ -60,36 +73,52 @@ def get_rotation_params(face):
             return ('Right', 'X', -1)
         case 'L':
             return ('Left', 'X', -1)
+        case 'L2':
+            return ('Left', 'X', -2)
         case "L'":
             return ('Left', 'X', 1)
         case 'F':
             return ('Front', 'Y', -1)
+        case 'F2':
+            return ('Front', 'Y', -2)
         case "F'":
             return ('Front', 'Y', 1)
         case 'B':
             return ('Back', 'Y', 1)
+        case 'B2':
+            return ('Back', 'Y', 2)
         case "B'":
             return ('Back', 'Y', -1)
         case 'U':
             return ('Up', 'Z', 1)
+        case 'U2':
+            return ('Up', 'Z', 2)
         case "U'":
             return ('Up', 'Z', -1)
         case 'D':
             return ('Down', 'Z', -1)
+        case 'D2':
+            return ('Down', 'Z', -2)
         case "D'":
             return ('Down', 'Z', 1)
-#        case 'M':
-#            return ('Left', 'X', -1)
-#        case "M'":
-#            return ('Left', 'X', 1)
-#        case 'S':
-#            return ('Front', 'Y', -1)
-#        case "S'":
-#            return ('Front', 'Y', 1)
-#        case 'E':
-#            return ('Down', 'Z', -1)
-#        case "E'":
-#            return ('Down', 'Z', 1)
+        case 'M':
+            return ('Left', 'X', -1)
+        case 'M2':
+            return ('Left', 'X', -2)
+        case "M'":
+            return ('Left', 'X', 1)
+        case 'S':
+            return ('Front', 'Y', -1)
+        case 'S2':
+            return ('Front', 'Y', -2)
+        case "S'":
+            return ('Front', 'Y', 1)
+        case 'E':
+            return ('Down', 'Z', -1)
+        case 'E2':
+            return ('Down', 'Z', -2)
+        case "E'":
+            return ('Down', 'Z', 1)
     return None
 
 # Function to rotate a face
@@ -103,20 +132,6 @@ def rotate_face(face):
             for obj in collection.objects:
                 obj.select_set(True)
             bpy.ops.transform.rotate(value=angle * dir, orient_axis=axis)
+    reset_collections()
 
-# Example rotations (accepts R', L', etc)
-rotate_face("R2")  # R 
-#rotate_face("L")  # L
-#rotate_face("F")  # F
-#rotate_face("B")  # B
-#rotate_face("U")  # U
-#rotate_face("D")  # D
-
-def scramble(scramble):
-    for move in range(0,len(scramble)-1):
-        if scramble[move+1] == ' ':
-            rotate_face(scramble[move])
-        else if scramble[move+1] == '2' or scramble[move+1] == "'":
-            rotate_face(scramble[move])
-            
-        else if s
+start()
